d5c3ec7f0c26a8540e1deb5d557442977ce3ace0
Sari Hammad <sari_hammad@hotmail.com>
2025-05-17 18:49:42 -0400

    feat: add persistence to rate limtiing service

--- src/main/java/com/devign/limitit/service/RateLimiterService.java
+++ src/main/java/com/devign/limitit/service/RateLimiterService.java
@@ -1,27 +1,42 @@
 package com.devign.limitit.service;
 
+import com.devign.limitit.entity.RateLimit;
+import com.devign.limitit.repository.RateLimitRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @RequiredArgsConstructor
 public class RateLimiterService {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RateLimitRepository rateLimitRepository;
     private static final long LIMIT = 100; // 100 reqs / min
     private static final long EXPIRY = 60; // 60 sec window
 
-    public boolean isAllowed(String key) {
-        Long current = redisTemplate.opsForValue().increment(key);
-        
-        if (current == 1) {
-            redisTemplate.expire(key, Duration.ofSeconds(EXPIRY));
+    public boolean isAllowed(String userId) {
+        RateLimit rateLimit = rateLimitRepository.findByUserId(userId)
+                .orElseGet(() -> createNewRateLimit(userId));
+
+        if (rateLimit.getExpiryTime().isBefore(LocalDateTime.now())) {
+            //Reset limit
+            rateLimit.setRequestCount(1L);
+            rateLimit.setExpiryTime(LocalDateTime.now().plus(EXPIRY, ChronoUnit.SECONDS));
+        } else {
+            rateLimit.setRequestCount(rateLimit.getRequestCount() + 1);
         }
 
-        return current <= LIMIT;
+        rateLimitRepository.save(rateLimit);
+        return rateLimit.getRequestCount() <= LIMIT;
+    }
+
+    private RateLimit createNewRateLimit(String userId) {
+        RateLimit newLimit = new RateLimit();
+        newLimit.setUserId(userId);
+        newLimit.setRequestCount(1L);
+        newLimit.setExpiryTime(LocalDateTime.now().plus(EXPIRY, ChronoUnit.SECONDS));
+        return rateLimitRepository.save(newLimit);
     }
 }
