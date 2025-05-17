da43301a52064d2d8e4a2aa9a05c535a947e141f
Sari Hammad <sari_hammad@hotmail.com>
2025-05-17 17:20:54 -0400

    feat: init Spring project

---
+++ src/main/java/com/limitit/limitit/LimititApplication.java
@@ -0,0 +1,13 @@
+package com.limitit.limitit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimititApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimititApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limitit/limitit/LimititApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limitit.limitit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimititApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
